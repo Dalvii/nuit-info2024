5cae29e5f36e9ad28bb3182fa2454b2a31bb7c56
Firelods <clement.lefevre1@etu.univ-cotedazur.fr>
2023-12-07 19:17:36 +0100

    ✨ Add answer functionality to Question component

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,7 @@ function App() {
 	const [listPlayer, setListPlayer] = useState<Player[]>([])
 	const [isLogged, setIsLogged] = useState(false)
 
+	const questiontest = true;
 
 	function login() {
 		socket.join(pseudo);
@@ -19,10 +20,22 @@ function App() {
 		})
 	}
 
+	function answer() {
+		console.log("answer")	
+	}
+	const listAnswers = [{
+		id: 1, text: "Reponse 1", color: "jaune"
+	}, {
+		id: 2, text: "Reponse 2", color: "rouge"
+	}, {
+		id: 3, text: "Reponse 3", color: "bleu"
+	}, {
+		id: 4, text: "Reponse 4", color: "vert"
+	}];
 	return (
 		<div>
 			{isLogged == true ?
-				<Question id={0} text={'Test Question'} answers={[]} />
+				<Question id={0} question={'Test Question'} answers={listAnswers} onAnswer={answer} />
 				:
 				<Login join={() => login()} pseudo={pseudo} setPseudo={setPseudo} />
 			}
