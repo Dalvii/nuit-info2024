9e0dd305ba0992fb72476db2812f70c568ad770e
Firelods <clement.lefevre1@etu.univ-cotedazur.fr>
2023-12-08 03:31:25 +0100

    Update CSS styles and socket URL

--- src/socket.ts
+++ src/socket.ts
@@ -3,7 +3,7 @@ import { joinedGame, Player } from './shared/types';
 
 // "undefined" means the URL will be computed from the `window.location` object
 // const URL = process.env.NODE_ENV === 'production' ? 'http://localhost:4000' : 'http://172.20.10.8:5001 ';
-const URL = 'http://172.20.10.8:5001/'
+const URL = 'http://localhost:5001/'
 console.log(URL);
 
 const socket = io(URL);
